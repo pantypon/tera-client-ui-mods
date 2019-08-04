f9765ddb0c7e16ec3002e0d99a3f2181faaf182e
Panty <yesterdaywastodayafterall@gmail.com>
2019-08-04 00:39:00 -1000

    Add files via upload

---
+++ Pink Exp Bar/index.js
@@ -0,0 +1,3 @@
+module.exports = function Pink_Exp_Bar(mod) {
+    mod.installGPK("S1UI_ExpBar.gpk");
+}
---
+++ Pink Main Menu/index.js
@@ -0,0 +1,3 @@
+module.exports = function Pink_Main_Menu(mod) {
+    mod.installGPK("S1UI_MainMenu.gpk");
+}
