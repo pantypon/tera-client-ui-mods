8b8372173b8af70915d1e5affc0c70c37249afe7
Panty <yesterdaywastodayafterall@gmail.com>
2019-08-04 01:32:40 -1000

    Add files via upload

---
+++ Pink Chat Window/index.js
@@ -0,0 +1,3 @@
+module.exports = function Pink_Chat_Window(mod) {
+    mod.installGPK("S1UI_Chat2.gpk");
+}
---
+++ Pink Skill Slots/index.js
@@ -0,0 +1,5 @@
+module.exports = function Pink_Skill_Slots(mod) {
+    mod.installGPK("S1UI_TestQuickSlot.gpk");
+    mod.installGPK("S1UI_TestQuickSlotEx.gpk");
+    mod.installGPK("S1UI_TestQuickSlotEx2.gpk");
+}
